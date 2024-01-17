720de42009c2fc03a2b0b7f6d7ab9f811a8fbb70
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-17 10:07:48 +0000

    folder setup

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Root Page (protected)</h1>
+    </div>
+  );
+};
+
+export default HomePage;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-red-500 text-6xl">Hello, world!</h1>
-    </div>
-  );
-}
