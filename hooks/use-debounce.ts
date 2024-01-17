6ebbed2d2325727e4e0bab5aab9001e5e7416396
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-17 11:50:06 +0000

    use debounce hook created

---
+++ hooks/use-debounce.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useDebounce<T>(value: T, delay: number = 500): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
