9f0a95638b81cc9bf47ea2a6549644810170f7c5
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-17 11:31:21 +0000

    navbar height fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -17,7 +17,7 @@ const font = Poppins({
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
         <Menu className="block md:hidden" />
         <Link href="/">
