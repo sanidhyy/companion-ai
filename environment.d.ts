a284a9cb9817832d4a2c9b610a7f50100870a875
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-18 10:09:23 +0000

    env types added

--- environment.d.ts
+++ environment.d.ts
@@ -9,6 +9,17 @@ declare global {
 
       // cloudinary cloud name
       NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: string;
+
+      // pinecone vector db credentials
+      PINECONE_INDEX: string;
+      PINECONE_API_KEY: string;
+
+      // upstash redis url and token
+      UPSTASH_REDIS_REST_URL: string;
+      UPSTASH_REDIS_REST_TOKEN: string;
+
+      // open ai key
+      OPENAI_API_KEY: string;
     }
   }
 }
