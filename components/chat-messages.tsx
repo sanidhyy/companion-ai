aa1905c0d65a4d648fb590f38b021e1695cb65ba
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-18 11:36:08 +0000

    fixed companion avatar

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -46,7 +46,7 @@ export const ChatMessages = ({
           key={message.content}
           role={message.role}
           content={message.content}
-          src={message.src}
+          src={companion.src}
         />
       ))}
 
