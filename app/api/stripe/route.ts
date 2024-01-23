a96cb8985a4a6505d421b094c02d0ac389e9b947
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-01-23 13:27:58 +0000

    stripe api route created

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,66 @@
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+import { stripe } from "@/lib/stripe";
+import { absoluteUrl } from "@/lib/utils";
+
+const settingsUrl = absoluteUrl("/settings");
+
+export async function GET() {
+  try {
+    const { userId } = auth();
+    const user = await currentUser();
+
+    if (!user || !userId)
+      return new NextResponse("Unauthorized.", { status: 401 });
+
+    const userSubscription = await db.userSubscription.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: settingsUrl,
+      });
+
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingsUrl,
+      cancel_url: settingsUrl,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: "USD",
+            product_data: {
+              name: "Companion Pro",
+              description: "Create Custom AI Companions.",
+            },
+            unit_amount: 999, // $9.99
+            recurring: {
+              interval: "month",
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    });
+
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+  } catch (error: unknown) {
+    console.error("[STRIPE_GET]: ", error);
+    return new NextResponse("Internal server error.", { status: 500 });
+  }
+}
