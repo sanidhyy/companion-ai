d5268119768ce1b4f46873c34299229667199531
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-09-11 19:27:03 +0530

    fix: typo

--- hooks/use-is-client.ts
+++ hooks/use-is-client.ts
@@ -2,6 +2,9 @@ import { useSyncExternalStore } from "react";
 
 const emptySubscribe = () => () => {};
 
-/** True only after client hydration; avoids SSR/client markup mismatches. */
 export const useIsClient = () =>
-  useSyncExternalStore(emptySubscribe, () => true, () => false);
+  useSyncExternalStore(
+    emptySubscribe,
+    () => true,
+    () => false,
+  );
