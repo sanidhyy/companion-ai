30d40387a0090911fb1b43692354bd809b846279
Sanidhya Kumar Verma <sanidhyyy@gmail.com>
2026-09-11 19:23:41 +0530

    fix: sign in/up page redirect

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/webhook(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/api/webhook(.*)",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
